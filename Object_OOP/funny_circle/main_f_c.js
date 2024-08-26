a05aa94e4cc92e6eda58d84c2a1f0f2868c78927
phamlong56 <mrphamlong56@gmail.com>
2024-08-26 13:16:46 +0700

    bt update funny circle

--- Object_OOP/funny_circle/main_f_c.js
+++ Object_OOP/funny_circle/main_f_c.js
@@ -16,20 +16,74 @@ class Circle{
     setY(y){
         this.y = y;
     }
-    getRadius(radius){
-        return this.radius;
+
+     getRandomHex(){
+        return Math.floor(Math.random() * 256);
     }
-    setRadius(radius){
-        this.radius = radius;
+    getRandomColor(){
+        let red= this.getRandomHex();
+        let green= this.getRandomHex();
+        let blue= this.getRandomHex();
+        return "rgb(" + red + "," + green + "," + blue + ")";
     }
 }
 
-    let ctx = document.getElementById("myCanvas").getContext("2d");
-    let radius= Math.floor(Math.random()*80)
-    let circle = new Circle(500,500,radius);
-    ctx.beginPath();
-    ctx.arc(circle.x, circle.y, circle.radius, 0, Math.PI*2, true);
-    ctx.fill();
+    // let x=200;
+    // let y=200;
+    // let ctx = document.getElementById("myCanvas").getContext("2d");
+    // let radius= Math.floor(Math.random()*80)
+    // let circle = new Circle(x,y,radius);
+    // let color= circle.getRandomColor();
+    // circle.setX(Math.random() * window.innerWidth);
+    // circle.setY(Math.random() * window.innerHeight);
+    // x=circle.getX();
+    // y=circle.getY();
+    //
+    // ctx.beginPath();
+    // ctx.arc(circle.x, circle.y, circle.radius, 0, Math.PI*2);
+    // ctx.fillStyle = color
+    // ctx.fill();
+
+// for (let i = 0; i < 10; i++) {
+//     let x=200;
+//     let y=200;
+//     let ctx = document.getElementById("myCanvas").getContext("2d");
+//     let radius= Math.floor(Math.random()*80)
+//     let circle = new Circle(x,y,radius);
+//     let color= circle.getRandomColor();
+//     circle.setX(Math.random() * window.innerWidth);
+//     circle.setY(Math.random() * window.innerHeight);
+//     x=circle.getX();
+//     y=circle.getY();
+//
+//     ctx.beginPath();
+//     ctx.arc(circle.x, circle.y, circle.radius, 0, Math.PI*2);
+//     ctx.fillStyle = color
+//     ctx.fill();
+let x=200;
+let y=200;
+let ctx = document.getElementById("myCanvas").getContext("2d");
+
+    function createMultipleCircle(){
+
+        let radius= Math.floor(Math.random()*80)
+        let circle = new Circle(x,y,radius);
+        let color= circle.getRandomColor();
+        circle.setX(Math.random() * window.innerWidth);
+        circle.setY(Math.random() * window.innerHeight);
+        x=circle.getX();
+        y=circle.getY();
+        ctx.beginPath();
+        ctx.arc(circle.x, circle.y, circle.radius, 0, Math.PI*2);
+        ctx.fillStyle = color
+        ctx.fill();
+    }
+    function drawMultipleCircles(number){
+        for (let i = 0; i < number; i++) {
+            createMultipleCircle()
+        }
+    }
+   drawMultipleCircles(Math.random()*100)
     console.log(circle)
 
 
